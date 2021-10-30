6ae436972d9dc747ae7b18f4f7a70fb4bc625918
Yaran66 <92379825+Yaran66@users.noreply.github.com>
2021-10-30 19:01:50 +0300

    Add files via upload

--- Makefile
+++ Makefile
@@ -6,16 +6,16 @@
 #   By: wjasmine <marvin@42.fr>                    +#+  +:+       +#+        #
 #                                                +#+#+#+#+#+   +#+           #
 #   Created: 2021/10/26 14:58:42 by wjasmine          #+#    #+#             #
-#   Updated: 202#   Updated: 2021/10/27 15:43:11 by wjasmine         ###   ########.fr       #                                                              #
+#   Updated: 202#   Updated: 2021/10/30 17:22:47 by wjasmine         ###   ########.fr       #                                                              #
 # ************************************************************************** #
 
 SRCS	=	ft_isalnum.c ft_toupper.c \
 			ft_isalpha.c ft_tolower.c \
-			ft_isascii.c \
-			ft_isdigit.c \
-			ft_isprint.c \
-			ft_memcpy.c \
-			ft_memmove.c \
+			ft_isascii.c ft_strchr.c \
+			ft_isdigit.c ft_strrchr.c \
+			ft_isprint.c ft_strncmp.c \
+			ft_memcpy.c ft_memchr.c \
+			ft_memmove.c ft_memcmp.c \
 			ft_memset.c \
 			ft_strlen.c \
 			ft_bzero.c \
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wjasmine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 16:25:36 by wjasmine          #+#    #+#             */
-/*   Updated: 2021/10/27 16:26:14 by wjasmine         ###   ########.fr       */
+/*   Updated: 2021/10/30 18:55:28 by wjasmine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -28,4 +28,9 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
